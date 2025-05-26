a3ac6618d45ce094c5b15dec028c9e3e030240a9
temple7447 <starukido@gmail.com>
2025-05-26 14:00:41 +0100

    sd

--- src/utils/currency.ts
+++
@@ -1,7 +0,0 @@
-export const formatCurrency = (amount: number) => {
-  return new Intl.NumberFormat('en-NG', {
-    style: 'currency',
-    currency: 'NGN',
-    minimumFractionDigits: 0
-  }).format(amount);
-};
